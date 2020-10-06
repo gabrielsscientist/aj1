78b3b59ae2ed141187021af7054f10b10898b0bb
gabrielsscientist <53708782+gabrielsscientist@users.noreply.github.com>
2020-10-06 13:34:43 -0300

    lab2

---
+++ DeclaracaoVariaveis.java
@@ -0,0 +1,14 @@
+public class DeclaracaoVariaveis {
+
+    public static void main(String[] args) {
+        String nome = "Gabriel Cassemiro";
+        int idade = 19;
+        String rg = "12.321.456-x";
+        char sexo = 'M';
+        double salario = 1500.0;
+        System.out.print(nome + ", de " + idade);
+        System.out.println(" anos, portador(a) do RG de número " + rg + ", ");
+        System.out.print("do sexo " + sexo);
+        System.out.println(", está registrado(a) com o salário de R$ " + salario);
+    }
+}
